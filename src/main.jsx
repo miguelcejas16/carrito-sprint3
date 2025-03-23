bf372ad31925314e3671b695f8e78b059a3e510e
miguelcejas16 <161471830+miguelcejas16@users.noreply.github.com>
2025-03-23 15:33:45 -0300

    correccion de nombre de archivo para hacer el deploy en netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "./App"
-import './index.css'
+import './index.css'//tenia mal el nombre
 import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
